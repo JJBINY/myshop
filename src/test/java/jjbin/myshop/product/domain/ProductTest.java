bdf9feb17b1affe8513cf20a1667f65f5150a46b
JJBINY <sisoya0424@gmail.com>
2024-02-02 02:46:28 +0900

    Test: product option and validation with order

--- src/test/java/jjbin/myshop/product/domain/ProductTest.java
+++ src/test/java/jjbin/myshop/product/domain/ProductTest.java
@@ -1,6 +1,6 @@
 package jjbin.myshop.product.domain;
 
-import jjbin.myshop.generic.domain.Money;
+import jjbin.myshop.FixtureBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,10 +13,7 @@ class ProductTest {
     @DisplayName("재고를 수량만큼 감소시킬 수 있다.")
     void can_reduce_stock() throws Exception {
         // given
-        Product sut = Product.builder()
-                .name("name")
-                .description("desc")
-                .price(Money.ZERO)
+        Product sut = FixtureBuilder.aProduct()
                 .stock(10)
                 .build();
 
@@ -31,15 +28,13 @@ class ProductTest {
     @DisplayName("재고가 충분하지 않으면 재고를 감소시킬 수 없다.")
     void can_not_reduce_stock_when_there_is_not_enough() throws Exception {
         // given
-        Product sut = Product.builder()
-                .name("name")
-                .description("desc")
-                .price(Money.ZERO)
-                .stock(5)
+        Product sut = FixtureBuilder.aProduct()
+                .stock(9)
                 .build();
 
         // when, then
         assertThatThrownBy(() -> sut.reduceStock(10))
                 .isInstanceOf(IllegalStateException.class);
     }
+
 }
