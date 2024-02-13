e36c3ee0ab7b6aa56fc97c78f154600b8f456955
JJBINY <sisoya0424@gmail.com>
2024-02-13 22:32:10 +0900

    Test: modify payment

--- src/test/java/jjbin/myshop/payment/domain/PaymentTest.java
+++ src/test/java/jjbin/myshop/payment/domain/PaymentTest.java
@@ -1,13 +1,10 @@
 package jjbin.myshop.payment.domain;
 
 import jjbin.myshop.generic.domain.Money;
-import jjbin.myshop.order.domain.Order;
-import jjbin.myshop.order.service.Cart;
-import jjbin.myshop.product.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static jjbin.myshop.order.domain.Order.OrderStatus.PAYED;
+import static jjbin.myshop.FixtureBuilder.aPayment;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,46 +14,36 @@ class PaymentTest {
     @DisplayName("현금으로 주문에 대한 결제를 할 수 있다.")
     void can_pay_for_an_order_in_cash() throws Exception {
         // given
-        Product product = Product.builder()
-                .name("name")
-                .description("desc")
-                .price(Money.wons(1000))
-                .stock(10)
-                .build();
-        Cart cart = new Cart(
-                new Cart.CartLineItem(product, 3),
-                new Cart.CartLineItem(product, 7));
-        Order order = Order.createOrder(cart);
         Cash cash = new Cash(Money.wons(10000));
-        Payment sut = new Payment(order, cash, order.calculateTotalPrice());
+        Money price = Money.wons(11000);
+        Payment sut = aPayment()
+                .orderAmount(price)
+                .discountAmount(Money.wons(1000))
+                .method(cash)
+                .build();
 
         // when
         sut.pay();
 
         // then
         assertThat(cash.getAmount()).isEqualTo(Money.ZERO);
-        assertThat(order.getOrderStatus()).isEqualTo(PAYED);
+        assertThat(sut.getStatus()).isEqualTo(Payment.PaymentStatus.COMPLETED);
     }
 
     @Test
     @DisplayName("잔액이 부족하면 결제가 실패한다.")
     void payment_will_fail_when_insufficient_balance() throws Exception {
         // given
-        Product product = Product.builder()
-                .name("name")
-                .description("desc")
-                .price(Money.wons(1000))
-                .stock(10)
+        Cash cash = new Cash(Money.wons(1000));
+        Money price = Money.wons(11000);
+        Payment sut = aPayment()
+                .orderAmount(price)
+                .discountAmount(Money.wons(1000))
+                .method(cash)
                 .build();
-        Cart cart = new Cart(
-                new Cart.CartLineItem(product, 3),
-                new Cart.CartLineItem(product, 7));
-        Order order = Order.createOrder(cart);
-        Cash cash = new Cash(Money.wons(9999));
-        Payment sut = new Payment(order, cash, order.calculateTotalPrice());
+
 
         // when, then
-        assertThatThrownBy(() -> sut.pay())
-                .isInstanceOf(IllegalStateException.class);
+        assertThatThrownBy(() -> sut.pay()).isInstanceOf(IllegalStateException.class);
     }
 }
