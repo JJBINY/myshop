34217aa9c8f3fbc81f812702510be27e0dd988e5
JJBINY <97151887+JJBINY@users.noreply.github.com>
2024-02-16 02:11:05 +0900

    Revert "Feat: product option and validation with order"
    This reverts commit b4677faae3ad497bc1020758d5e1e2faba46a42e.

--- src/main/java/jjbin/myshop/order/domain/OrderLineItem.java
+++ src/main/java/jjbin/myshop/order/domain/OrderLineItem.java
@@ -2,66 +2,20 @@ package jjbin.myshop.order.domain;
 
 import jjbin.myshop.generic.domain.Money;
 import jjbin.myshop.product.domain.Product;
-import lombok.Builder;
-import lombok.NonNull;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-import static java.util.Objects.requireNonNull;
 
 public class OrderLineItem {
     private Product product;
-    private String name;
     private int quantity;
-    private List<OrderOptionGroup> orderOptionGroups = new ArrayList<>();
 
-    @Builder
-    public OrderLineItem(@NonNull Product product, @NonNull String name, int quantity, @NonNull List<OrderOptionGroup> optionGroups) {
+    public OrderLineItem(Product product, int quantity) {
         this.product = product;
-        this.name = name;
         this.quantity = quantity;
-        this.orderOptionGroups.addAll(optionGroups);
-
-        checkInvariant();
     }
 
-    private void checkInvariant() {
-        requireNonNull(product);
-        requireNonNull(name);
-
-        if(quantity<=0){
-            throw new IllegalStateException("주문 수량은 1개 이상이어야 합니다.");
-        }
-
-        if(orderOptionGroups.isEmpty()){
-            throw new IllegalStateException("주문 옵션 그룹은 필수입니다.");
-        }
-    }
-
-    public void place(){
+    public void placeOrder(){
         product.reduceStock(quantity);
     }
-
     public Money calculatePrice(){
-        Money price = Money.sum(orderOptionGroups, OrderOptionGroup::calculatePrice).times(quantity);
-
-        assert price.isGreaterThanOrEqual(Money.ZERO);
-        return price;
-    }
-
-    void validate() {
-        if (!Objects.equals(name, product.getName())) {
-            throw new IllegalStateException("상품명이 변경되었습니다.");
-        }
-
-        if(!product.canReduceStock(quantity)){
-            throw new IllegalStateException("상품 재고가 부족합니다.");
-        }
-
-        for (OrderOptionGroup group : orderOptionGroups) {
-            group.validate(product.getOptionGroupSpecs());
-        }
+        return product.getPrice().times(quantity);
     }
 }
--- src/main/java/jjbin/myshop/payment/domain/Cash.java
+++ src/main/java/jjbin/myshop/payment/domain/Cash.java
@@ -1,9 +1,10 @@
 package jjbin.myshop.payment.domain;
 
 import jjbin.myshop.generic.domain.Money;
+import lombok.Getter;
 
 public class Cash implements PaymentMethod {
-
+    @Getter
     private Money amount;
 
     public Cash(Money amount) {
@@ -12,19 +13,9 @@ public class Cash implements PaymentMethod {
 
     @Override
     public void pay(Money amount) {
-        if (amount.isLessThan(Money.ZERO)) {
-            throw new IllegalArgumentException("결제 금액은 0원 이상이어야 합니다.");
-        }
         if (this.amount.isLessThan(amount)) {
             throw new IllegalStateException("잔액이 부족합니다.");
         }
-
         this.amount = this.amount.minus(amount);
-
-        assert this.amount.isGreaterThanOrEqual(Money.ZERO);
-    }
-
-    public Money getAmount() {
-        return amount;
     }
 }
