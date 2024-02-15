837d68b71cde0988acd01a9ad1cee7e4f761c6a8
JJBINY <97151887+JJBINY@users.noreply.github.com>
2024-02-16 02:11:05 +0900

    Revert "Test: product option and validation with order"
    This reverts commit ca7abb5b8f8ac3209df42736a64d1e7cd3b48968.

--- src/test/java/jjbin/myshop/product/domain/ProductTest.java
+++ src/test/java/jjbin/myshop/product/domain/ProductTest.java
@@ -1,6 +1,6 @@
 package jjbin.myshop.product.domain;
 
-import jjbin.myshop.FixtureBuilder;
+import jjbin.myshop.generic.domain.Money;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +13,10 @@ class ProductTest {
     @DisplayName("재고를 수량만큼 감소시킬 수 있다.")
     void can_reduce_stock() throws Exception {
         // given
-        Product sut = FixtureBuilder.aProduct()
+        Product sut = Product.builder()
+                .name("name")
+                .description("desc")
+                .price(Money.ZERO)
                 .stock(10)
                 .build();
 
@@ -28,13 +31,15 @@ class ProductTest {
     @DisplayName("재고가 충분하지 않으면 재고를 감소시킬 수 없다.")
     void can_not_reduce_stock_when_there_is_not_enough() throws Exception {
         // given
-        Product sut = FixtureBuilder.aProduct()
-                .stock(9)
+        Product sut = Product.builder()
+                .name("name")
+                .description("desc")
+                .price(Money.ZERO)
+                .stock(5)
                 .build();
 
         // when, then
         assertThatThrownBy(() -> sut.reduceStock(10))
                 .isInstanceOf(IllegalStateException.class);
     }
-
 }
